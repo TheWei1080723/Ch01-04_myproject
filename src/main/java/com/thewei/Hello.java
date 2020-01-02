76905fd565cc768759266ae4fb4f8bae6d71c3d7
TheWei <chihwei1080723@gmail.com>
2020-01-02 11:31:27 +0800

    Java and Kotlin vaiable, data type

--- src/main/java/com/thewei/Hello.java
+++ src/main/java/com/thewei/Hello.java
@@ -5,5 +5,22 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+
+        int age = 33;
+        Integer age2 = 33;
+        age2.byteValue();
+
+        char c = 'A';
+        Character c2 = 'A';
+
+        byte b = 120;
+        float  wejght = 66.5f;
+        Float wejght2 = 66.5f;
+        wejght2.byteValue();
+
+        double height = 1.7;
+        boolean adult = true;
+        String name = "蔡志偉";
+
     }
 }
--- src/main/kotlin/com/thewei/Hello.kt
+++ src/main/kotlin/com/thewei/Hello.kt
@@ -2,9 +2,16 @@ package com.thewei
 
 fun main(args: Array<String>) {
 //    println("Hello Kotlin");
-//    Human().hello()
+    Human().hello()
     val h = Human();
     h.hello();
+
+    var age : Int = 19;
+    age = 20;
+    var weight = 66.5f;
+    val name = "The Wei";
+    var enname :String;
+    enname = "Wei";
 }
 
 class Human{
